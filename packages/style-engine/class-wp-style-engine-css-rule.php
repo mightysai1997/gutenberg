8234a5cd6c43ec3f9e5a4e761561e11105e01eca
Ari Stathopoulos <aristath@gmail.com>
2022-08-02 14:20:59 +0300

    Use the processor to add styles

--- packages/style-engine/class-wp-style-engine-css-rule.php
+++ packages/style-engine/class-wp-style-engine-css-rule.php
@@ -117,6 +117,11 @@ class WP_Style_Engine_CSS_Rule {
 		$declarations_indent = $should_prettify ? $indent_count + 1 : 0;
 		$new_line            = $should_prettify ? "\n" : '';
 		$space               = $should_prettify ? ' ' : '';
+        
+		$styles = $this->declarations->get_declarations_string();
+		if ( empty( $styles ) ) {
+			return '';
+		}
 		return $rule_indent . $this->get_selector() . "{$space}{{$new_line}" . $this->declarations->get_declarations_string( $should_prettify, $declarations_indent ) . "{$new_line}{$rule_indent}}";
 	}
 }
